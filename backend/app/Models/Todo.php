ec04bf8d4af05fac56b1798fc522b2a18d91b523
salehibneomar <salehibneomar@gmail.com>
2025-05-20 01:55:00 +0600

    Fix seeder-related issue by adding HasFactory trait to Todo model

--- backend/app/Models/Todo.php
+++ backend/app/Models/Todo.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Todo extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
+
     protected $fillable = ['title', 'description', 'completed'];
+
+    protected $dates = ['deleted_at'];
 }
