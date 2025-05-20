1c88b16acd56b0f1dbf842a4565944d42e483078
Saleh Ibne Omar <35599978+salehibneomar@users.noreply.github.com>
2025-05-21 02:37:11 +0600

    Bugfix/error response fx (#6)
    * added list api
    
    * fixed
    
    * fixed

---
+++ backend/app/Enums/CrudStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum CrudStatus: string
+{
+    case CREATED = 'Successfully Created';
+    case DELETED = 'Successfully Deleted';
+    case UPDATED = 'Successfully Updated';
+    case NO_CHANGE = 'No Change';
+}
