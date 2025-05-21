3435fb9160640905d3bdb7e2d8ee3b21c7103639
salehibneomar <salehibneomar@gmail.com>
2025-05-22 03:32:45 +0600

    indentation

--- backend/app/Http/Requests/BaseRequest.php
+++ backend/app/Http/Requests/BaseRequest.php
@@ -9,20 +9,14 @@ use App\Enums\HttpStatus;
 use Illuminate\Validation\ValidationException;
 
 /**
- * Class BaseRequest
- *
  * This class extends Laravel's FormRequest and provides a centralized way to handle validation failures.
- * It uses the ApiResponserTrait to format validation error responses consistently across the application.
  */
 class BaseRequest extends FormRequest
 {
     use ApiResponserTrait;
 
     /**
-     * Handle a failed validation attempt.
-     *
-     * This method is triggered when validation fails for a request. It overrides the default behavior
-     * to provide a custom error response using the `errorResponse` method from the ApiResponserTrait.
+     * @override the 'failedValidation' the default method of FormRequest
      * @param Validator
      * @throws ValidationException
      */
